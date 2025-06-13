64d815cbc0c74c32771f636ebfafcc23cb542a17
oksmantv <aleoks_1@hotmail.com>
2025-06-14 00:38:33 +0200

    Updated Powershell Scripts

--- MissionUpdateBat/UpdateScripts/Show-ManualCopyInstructions.ps1
+++ MissionUpdateBat/UpdateScripts/Show-ManualCopyInstructions.ps1
@@ -1,12 +1,9 @@
 Write-Host ""
 Write-Host "Please COPY and PASTE the following files and folders from your framework folder into this folder:"
-Write-Host " - script_component.hpp"
-Write-Host " - onPlayerRespawn.sqf"
-Write-Host " - initPlayerLocal.sqf"
-Write-Host " - init.sqf"
 Write-Host " - Description.ext"
 Write-Host " - Modules (folder)"
 Write-Host " - Core (folder)"
+Write-Host "NOTE! If you have custom kits, best save them separately and then overwrite again or use GitHub Desktop and undo the change once applied."
 Write-Host ""
 Write-Host "Press ENTER when done."
 [void][System.Console]::ReadLine()
